c951bf3c1dec1727f374fd5c5e78b29071de7a1f
Jagwinder Singh <jagsingh@hotmail.co.nz>
2024-10-11 12:17:41 +1300

    added deploy workflows

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FBDfrontend-vite-deploy/',
   plugins: [react()],
   define: {
     global: {},
